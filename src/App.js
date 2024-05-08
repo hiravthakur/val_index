6b868cbc6da6e1abf635192048baef1355254c44
Hirav T <hirav.a.s.thakur@gmail.com>
2024-05-08 14:13:37 -0400

    began development

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>I like men</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+      <center>
+        <strong>
+           The Valorant Index
+        </strong>
+      </center>
+      </h1>
     </div>
   );
 }
