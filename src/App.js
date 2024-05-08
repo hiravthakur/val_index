ee8301c67aac6e805dcf755b2edb63c92cb89f6c
Hirav T <hirav.a.s.thakur@gmail.com>
2024-05-08 13:10:19 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>I like men</code> and save to reload.
         </p>
         <a
           className="App-link"
