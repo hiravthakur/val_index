b48149fabde09a909799cd5c99a69ed70d9ae0fc
Hirav T <hirav.a.s.thakur@gmail.com>
2024-05-11 16:32:30 -0400

    added a flexbox and mapped out all characters

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@
 import './App.css';
 import CharDeployed from './characterContainer.js';
 import SearchAppBar from './searchAppBar';
+import characters from './characters.json';
+
 function App() {
   return (
     <>
       <SearchAppBar />
-      <CharDeployed name="Hirav" imageSrc={"https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt6577b1f58530e6b2/5eb7cdc121a5027d77420208/V_AGENTS_587x900_Reyna.png"} age="25" role="Duelist"/>
+      <div className="containsGrid">
+      {characters.map((item) => ( <div className="itemName"> <CharDeployed name={item.name} imageSrc={item.imageSrc} realn={item['real-name']} role={item.role}/>
+      </div>))}
+      </div>
       </>
   );
 }
--- src/characterContainer.js
+++ src/characterContainer.js
@@ -1,24 +1,25 @@
 import * as React from 'react';
-import Stack from '@mui/material/Stack';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
 
 //Creates a container for the characters from MUI
 const DemoPaper = styled(Paper)(({ theme }) => ({
-  width: 160,
-  height: 250,
+  width: 170,
+  height: 320,
   padding: theme.spacing(3),
   ...theme.typography.body2,
   textAlign: 'center',
 }));
 
-export default function CharDeployed({name, role, age, imageSrc}) {
+export default function CharDeployed({name, role, realn, imageSrc}) {
     return (
-        <DemoPaper square={false}>{name}
+        <DemoPaper square={false}> <p>
+            <strong>{name}</strong>
+            </p>
         <img src={imageSrc} style={{width: '140px', height: '180px'}}/>
-        Age: {age} <p>
+        <p>
+        Real Name: {realn} </p>
         Role: {role}
-        </p>
         </DemoPaper>
     );
   }
