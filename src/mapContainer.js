00eb0a1aa4a3ba4d8007f6ac784f69d513389ce9
Hirav T <hirav.a.s.thakur@gmail.com>
2024-05-24 21:15:09 -0400

    map containers formatted

---
+++ src/mapContainer.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import BasicModal from './displayModal';
+//Creates a container for the characters from MUI
+const DemoPaper = styled(Paper)(({ theme }) => ({
+  width: 170,
+  height: 340,
+  padding: theme.spacing(3),
+  ...theme.typography.body2,
+  textAlign: 'center',
+  borderRadius: 20,
+}));
+
+export default function MapDeployed({name, location, deatchmatch, laysrc}) {
+    return (
+        <BasicModal>
+            <DemoPaper square={false}>
+                <span align="right">
+                </span>
+                <p>
+                <strong>{name}</strong>
+                </p>
+            <img src={laysrc} alt='' style={{width: '140px', height: '200px'}}/>
+            <p>
+            Location: {location} </p>
+            Deathmatch placeholder
+            </DemoPaper>
+        </BasicModal>
+    );
+  }
